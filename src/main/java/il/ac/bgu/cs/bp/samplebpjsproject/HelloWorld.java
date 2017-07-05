6a3d15fefaa0370708b726e84c9c15785d64612b
Michael Bar-Sinai <mich.barsinai@gmail.com>
2017-07-05 12:20:09 +0300

    Updated for the new API

--- src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
@@ -1,5 +1,6 @@
 package il.ac.bgu.cs.bp.samplebpjsproject;
 
+import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.BProgramRunner;
 import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.SingleResourceBProgram;
 import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.listeners.StreamLoggerListener;
 
@@ -11,13 +12,16 @@ public class HelloWorld {
     
     public static void main(String[] args) throws InterruptedException {
         // This will load the program file  <Project>/src/main/resources/HelloBPjsWorld.js
-        final SingleResourceBProgram bprog = new SingleResourceBProgram("HelloBPjsWorld.js");
+        final SingleResourceBProgram bprog = 
+                   new SingleResourceBProgram("HelloBPjsWorld.js");
         
+        BProgramRunner rnr = new BProgramRunner(bprog);
+
         // Print program events to the console
-        bprog.addListener( new StreamLoggerListener() );
+        rnr.addListener( new StreamLoggerListener() );
         
         // go!
-        bprog.start();
+        rnr.start();
     }
     
 }
